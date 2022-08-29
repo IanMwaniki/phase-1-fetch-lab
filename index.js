8f29f9b21485b3f29eef41345f0aa44ca11cca4e
Ian Maina Mwaniki <ian.mwaniki@student.moringaschool.com>
2022-08-29 23:16:27 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  let books;
+books= fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));
+ 
+  return books;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  var main = document.querySelector('main');
+  books.forEach(function (book) {
+    var h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
+
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
